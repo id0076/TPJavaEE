42cfe239fccb65bc455d159890a24c4f5be91914
id0158 <id0158@vasile-3363102>
2015-12-14 14:41:25 +0100

    ModificationUtilisateur

--- src/main/java/ensai/ProjetJavaEE/utilisateurs/services/ModificationUtilisateurService.java
+++ src/main/java/ensai/ProjetJavaEE/utilisateurs/services/ModificationUtilisateurService.java
@@ -1,5 +1,7 @@
 package ensai.ProjetJavaEE.utilisateurs.services;
 
+import javax.persistence.EntityManager;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -12,20 +14,15 @@ public class ModificationUtilisateurService {
 	
 	@Autowired
 	private RechercherUtilisateurService rechercheUtilisateurService;
-	
+
 	@Autowired
-	private CreationUtilisateurService creationUtilisateurService;
+	private EntityManager entityManager;
 
 	@Transactional(propagation = Propagation.REQUIRED)
 	public void ModificationEmailUtilisateur(String nouveauEmail, String login) {
 		Utilisateur utilisateur = rechercheUtilisateurService.rechercherParLogin(login);
 		utilisateur.setEmail(nouveauEmail);
-		try {
-			creationUtilisateurService.creer(utilisateur);
-		} catch (UtilisateurInvalideException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		entityManager.persist(utilisateur);
 	}
 
 }
