ab88da8f0415540623d9ff56a01da26f24fe2fdd
id0158 <id0158@vasile-3363102>
2015-12-16 12:36:53 +0100

    Modification de la classe NotificationsServices

--- src/main/java/ensai/ProjetJavaEE/VoteApp.java
+++ src/main/java/ensai/ProjetJavaEE/VoteApp.java
@@ -3,10 +3,16 @@ package ensai.ProjetJavaEE;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class VoteApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VoteApp.class, args);
+		int i = 777;
+		log.info("Hello World {}", i);
+		
 	}
 }
